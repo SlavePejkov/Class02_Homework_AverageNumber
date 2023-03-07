f6aeb682c76621a85240f7fbdbd79ac8302b1010
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2023-03-07 09:41:06 +0100

    Add files via upload

---
+++ AverageNumber/AverageNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter four numbers:");
+
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+            string inputThree = Console.ReadLine();
+            string inputFour = Console.ReadLine();
+
+            int intInputOne;
+            int intInputTwo;
+            int intInputThree;
+            int intInputFour;
+            
+
+            bool parsedInputOne = int.TryParse(inputOne, out intInputOne);
+            bool parsedInputTwo = int.TryParse(inputTwo, out intInputTwo);
+            bool parsedInputThree = int.TryParse(inputThree, out intInputThree);
+            bool parsedInputFour = int.TryParse(inputFour, out intInputFour);
+
+            double result = Convert.ToDouble(intInputOne + intInputTwo + intInputThree + intInputFour) / 4;
+
+            Console.WriteLine($"The average of {intInputOne} {intInputTwo} {intInputThree} {intInputFour} is: {result}");
+
+        }
+    }
+}
